7b4ae404a18aef7a4c6c5b54103023c173986d82
Hieu Tran <hieutrantrung.it@gmail.com>
2021-07-28 22:18:34 +0700

    multi threading

--- crawler/ckanseedcrawl.py
+++ crawler/ckanseedcrawl.py
@@ -1,12 +1,15 @@
 import json, requests
+import _thread
 from datetime import datetime
 
-def getPackagesList():
+def fetchBulkPackagesList():
     url = "https://datasets.seed.nsw.gov.au/api/3/action/package_list"
     response = requests.get(url)
-    slice_len = 10
+    slice_len = 100
     temp_list = []
-    for i, package_name in enumerate(response.json()["result"]):
+    packages = response.json()["result"]
+    print("number of package {}".format(len(packages)))
+    for i, package_name in enumerate(packages):
         if (i+1)%slice_len == 0:
             yield temp_list
             temp_list = []
@@ -18,13 +21,21 @@ def getPackageMetadata(package_name):
     response = requests.get(url)
     return response.json()
 
-for packages_list in getPackagesList():
-    print("packages_list - {0}".format(packages_list))
+def getPackagesMetadata(postfix, packages_list):
+    print("packages_list number - {0}".format(postfix))
+    print("packages_list length - {0}".format(len(packages_list)))
     partition = []
     for package_name in packages_list:
         data = getPackageMetadata(package_name)
-        print("packages_data - {0}".format(data))
+        print("packages_data - {0}".format(package_name))
         partition.append(data)
-    print("writing file ...")
-    with open("seed_"+datetime.now().strftime("%m-%d-%Y-%H:%M:%S")+".json", 'w') as outfile:
+    with open("seed_"+str(postfix)+".json", 'w') as outfile:
         json.dump(partition, outfile)
+
+postfix_number = 0
+for packages_list in fetchBulkPackagesList():
+    _thread.start_new_thread(getPackagesMetadata, (postfix_number, packages_list))
+    postfix_number+=1
+
+while 1:
+    pass
