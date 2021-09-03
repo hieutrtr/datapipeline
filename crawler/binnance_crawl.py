17fed98ed21b84df9722df042b4dc855da79f01e
Hieu Tran <hieutrantrung.it@gmail.com>
2021-09-03 21:39:59 +0700

    crawling trade record of all symbols on binance

--- crawler/binnance_crawl.py
+++ crawler/binnance_crawl.py
@@ -2,13 +2,14 @@ import requests as rq
 import pandas as pd
 from sys import argv
 import time
+import _thread
 binance_host = 'https://api.binance.com'
 binance_api_price = '/api/v3/ticker/price'
 binance_api_trades = '/api/v3/trades'
 binance_path_root = '/root/data/pipeline/binance'
 binance_path_symbols_price = '/symbols/price'
 binance_path_symbols_trades = '/symbols/trades'
-file_record_limit = 500
+file_record_limit = 1000
 def store_trades_as_parquet(data, symbol):
     partition_cols = ['symbol', 'year', 'month', 'day', 'hour']
     df = pd.json_normalize(data)
@@ -19,33 +20,39 @@ def store_trades_as_parquet(data, symbol):
     print(df.dtypes)
     df.to_parquet("{}{}".format(binance_path_root, binance_path_symbols_trades), partition_cols=partition_cols)
 
+def get_trade(symbol):
+    largest_trade_ids = 0
+    trade_data = []
+    while True:
+        time.sleep(60)
+        try:
+            res = rq.get("{}{}?symbol={}".format(binance_host, binance_api_trades, symbol))
+            trades = res.json()
+            if largest_trade_ids != 0:
+                for trade in trades[::-1]:
+                    if trade['id'] == largest_trade_ids:
+                        break
+                    trade_data.append(trade)
+                if trades[-1]['id'] > largest_trade_ids:
+                    largest_trade_ids = trades[-1]['id']
+            else:
+                trade_data = trades
+                largest_trade_ids = trades[-1]['id']
+        except Exception as e:
+            print('Exception: {}'.format(e))
+
+        if len(trade_data) >= file_record_limit:
+            store_trades_as_parquet(trade_data, symbol)
+            trade_data = []
 
 def crawl_trades():
     prices = rq.get("{}{}".format(binance_host, binance_api_price))
-    largest_trade_ids = {}
-    trades_data = {}
+    print('number of symbols: {}'.format(len(prices.json())))
+    for price in prices.json():
+        time.sleep(2)
+        _thread.start_new_thread(get_trade, (price['symbol'],))
     while(True):
-        time.sleep(10)
-        for price in prices.json():
-            try:
-                symbol = price['symbol']
-                res = rq.get("{}{}?symbol={}".format(binance_host, binance_api_trades, symbol))
-                trades = res.json()
-                if largest_trade_ids.get(symbol, 0) != 0:
-                    for trade in trades[::-1]:
-                        if trade['id'] == largest_trade_ids.get(symbol, 0):
-                            break
-                        trades_data.get(symbol, []).append(trade)
-                    if trades[-1]['id'] > largest_trade_ids.get(symbol, 0):
-                        largest_trade_ids[symbol] = trades[-1]['id']
-                else:
-                    trades_data[symbol] = trades
-                    largest_trade_ids[symbol] = trades[-1]['id']
-            except Exception as e:
-                print('Exception: {}'.format(e))
-        for symbol in trades_data.keys():
-            if len(trades_data[symbol]) >= file_record_limit:
-                store_trades_as_parquet(trades_data[symbol], symbol)
+        time.sleep(1000)
 
 def crawl_symbols():
     res = rq.get("{}{}".format(binance_host, binance_api_price))
